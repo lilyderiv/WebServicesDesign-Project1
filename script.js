2133ce463368c81bbf6529e5e8b9018ff4fc3f3a
lilyderiv <busrakubradereli@gmail.com>
2025-07-23 06:26:34 +0200

    kubra/final version of project

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const userName = prompt("Welcome! What's your name?");
+  if (userName) {
+    alert(`Hello ${userName}, enjoy exploring my ERASMUS journey!`);
+  }
+});
